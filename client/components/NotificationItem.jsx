1a3d13b653aacb983c28914f3e76e7e21c202bef
DuyetVu2001 <vungocduyet24@gmail.com>
2021-10-08 21:51:19 +0700

    add notifacation dropdown menu

--- client/components/NotificationItem.jsx
+++ client/components/NotificationItem.jsx
@@ -1,29 +1,37 @@
 import Image from 'next/image';
+import TopNavIcon from './TopNavIcon';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
 export default function NotificationItem({
-	channel,
+	channelImg,
 	content,
 	time,
 	thumbnail,
 }) {
+	console.log(thumbnail);
+
 	return (
-		<div className="">
-			<div className="relative w-[38px] h-[38px]">
+		<div className="flex items-start py-4 hover:bg-[#0000000d]">
+			<div className="relative w-[48px] h-[48px] mx-4">
 				<Image
 					className="rounded-full object-cover"
-					src={channel}
+					src={channelImg}
 					layout="fill"
 				/>
 			</div>
 
-			<div className="">
-				<div className="">{content}</div>
-				<div className="">{time}</div>
+			<div className="flex-1">
+				<div className="font-medium text-sm">{content}</div>
+				<div className="pt-1 text-[13px] text-gray-color">
+					{time} minutes ago
+				</div>
 			</div>
 
-			<div className="relative w-[86px]">
-				<Image className="object-cover" src={thumbnail} layout="fill" />
+			<div className="relative w-[86px] h-[50px] border">
+				<Image className="w-full object-cover" src={thumbnail} layout="fill" />
 			</div>
+
+			<TopNavIcon Icon={BsThreeDotsVertical} />
 		</div>
 	);
 }

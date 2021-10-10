c4f51d7463634f8592b7619bee3d335e92fea77d
DuyetVu2001 <vungocduyet24@gmail.com>
2021-10-10 17:52:03 +0700

    fix

--- client/pages/detail.jsx
+++ client/pages/detail.jsx
@@ -17,10 +17,10 @@ export default function Detail() {
 								height="720"
 								src="https://www.youtube.com/embed/iORWC-hZeX8"
 								title="YouTube video player"
-								frameborder="0"
+								frameBorder="0"
 								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-								allowfullscreen
-							></iframe>
+								allowFullScreen
+							/>
 						</div>
 					</div>
 					<div className="w-[402px]">
